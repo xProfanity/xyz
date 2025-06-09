1e274f7813c73dd1dc22193e1019b0a6d1502bda
xprofanity <pecpalmplay@gmail.com>
2025-06-09 18:24:48 +0200

    ...

--- app/lecture/[_id]/page.tsx
+++ app/lecture/[_id]/page.tsx
@@ -4,15 +4,11 @@ import React from 'react'
 import LectureDetails from '../Lecture'
 
 export async function generateStaticParams() {
-    try {
         const lectures = await fetchLectures() as Lecture[]
         
         return lectures.map((lecture) => ({
             _id: lecture._id,
         }))
-    } catch (error) {
-        console.log('error', error)
-    }
 }
 
 export default async function Layout({params}: {params: Promise<{_id: string}>}) {
