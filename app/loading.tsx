9e14b3042b514d3644c2cd339e3ae6dd001207f4
xprofanity <pecpalmplay@gmail.com>
2025-06-20 02:34:00 +0200

    styled resources cards

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Loading() {
   return (
-    <div>loading....</div>
+    <div className='h-screen flex flex-col justify-center items-center w-full max-w-xl'>
+      <div>
+        <Image
+          src={"/logo.png"}
+          height={250}
+          width={320}
+          alt='logo'
+          className='object-cover'
+        />
+      </div>
+
+      <p className='mt-4 text-gray-400'>Loading...</p>
+    </div>
   )
 }
--- app/student/books/page.tsx
+++ app/student/books/page.tsx
@@ -3,9 +3,11 @@
 import { fetchResourcesByEducationType } from '@/services/sanity'
 import { useTabs, useUser } from '@/store'
 import { Resource } from '@/types'
+import dayjs from 'dayjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
+import { BiDownload } from 'react-icons/bi'
 import Skeleton from 'react-loading-skeleton'
 import { toast } from 'sonner'
 
@@ -41,7 +43,7 @@ export default function Books() {
       <div className="flex flex-row flex-wrap justify-center items-center md:justify-normal md:items-start gap-4 w-full mt-4">
           {resources?.map((resource, index) => (
             resource.document.cover.coverUrl ? (
-              <Link href={`${resource.document.cover.fileUrl}/?dl=lecture-${resource.document.cover.fileName}`} className="relative rounded-lg cursor-pointer">
+              <div className="relative rounded-lg p-2 bg-sky-300">
                 <div className="relative h-[200px] w-[350px]">
                   <Image
                     src={resource.document.cover.coverUrl!}
@@ -50,11 +52,28 @@ export default function Books() {
                     className="object-fill rounded-lg"
                   />
                 </div>
-              </Link>
+
+                <div className='mt-2 w-full px-2 flex flex-row justify-between items-center'>
+                  <h1 className='text-lg font-semibold'>{resource.title}</h1>
+                </div>
+
+                <div className='w-full flex flex-col justify-start items-start gap-2 px-2 mt-1'>
+                  <p className=''>{resource.document.document.notes}</p>
+                  <p className='text-gray-600 text-sm'>{resource.document.document.description}</p>
+
+                  <div className='mt-4 flex flex-row justify-between items-center w-full'>
+                    <span className='text-gray-700 text-xs'>{dayjs(resource._createdAt).format("DD MMMM, YYYY")}</span>
+
+                    <Link href={`${resource.document.cover.fileUrl}/dl?=${educationType === "professional" ? "lecture" : "class"}-${resource.document.cover.fileName}`} className='cursor-pointer'>
+                      <BiDownload size={35} />
+                    </Link>
+                  </div>
+                </div>
+
+              </div>
             ) : (
               <Link href={`${resource.document.cover.fileUrl}/?dl=lecture-${resource.document.cover.fileName}`} key={index} className="w-[300px] rounded-lg bg-gray-300 hover:bg-gray-300/70 cursor-pointer flex flex-col justify-between p-4">
                 <h1>{resource.title}</h1>
-  
                 {/* <div>
                   <div className="mt-2">
                     <p className="font-bold text-lg">{resource..subject || question.course}</p>
